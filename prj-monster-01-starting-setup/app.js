8068568269523e5d3f17cfd3867eb31f33106158
Sumana <sumana.sattar@gmail.com>
2021-09-03 16:02:19 +0200

    adding restart and result to the game

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -8,33 +8,62 @@ const app = Vue.createApp({
             monsterHealth:100,
             playerHealth:100,
             counter:0,
-            a=false
+            a:false,
+            gameEnd:false
         }
     
     },
     watch:{
         counter() {
             if(this.counter ===3) {
-                this.a=true;
+               
+                this.a=!this.a;
+                this.counter=0;
             }
         }
     },
+    computed:{
+          setValue() {
+             
+              return this.a;
+          },
+          
+    },
     methods: {
+        
+        
         playerAttack() {
-            this.monsterHealth= this.monsterHealth- randomValue(5,10);
-            console.log(this.monsterHealth);
-            this.monsterAttack();
-            counter++;
+            if(this.monsterHealth <= 0){
+                    this.monsterHealth=0;
+                    this.result(player);
+                    this.gameEnd=true;
+            }else{
+                this.monsterHealth= this.monsterHealth- randomValue(5,10);
+                console.log(this.monsterHealth);
+                this.monsterAttack();
+                this.counter++;
+            }
+           
+           
         },
         monsterAttack() {
+            
             this.playerHealth =this.playerHealth- randomValue(2,9);
             console.log(this.playerHealth);
+        },
+        startAgain() {
+            this.monsterHealth=100;
+            this.playerHealth=100;
+            this.counter=0;
+            this.a=false;
+            this.gameEnd=false;
         },
         specialAttack() {
             this.monsterHealth=this.monsterHealth-randomValue(10,20);
             this.monsterAttack();
-            counter++;
+            this.counter++;
         }
     }
+    
 });
 app.mount('#game');
