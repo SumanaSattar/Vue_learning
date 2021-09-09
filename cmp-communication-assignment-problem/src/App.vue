7ce12c7d0e710030e9d71e5572b1af2356792d04
Sumana <sumana.sattar@gmail.com>
2021-09-09 11:43:46 +0200

    adding components in vue

---
+++ cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <section>
+        <h1>working with vue framework </h1>
+    <active-user></active-user>
+    <user-data></user-data>
+    </section>
+    
+</template>
+<script>
+
+export default{
+ 
+
+}
+</script>
+<style>
+
+</style>
